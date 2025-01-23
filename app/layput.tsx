87160a271e197484a39ee883a5377250a49ed3b2
takeshun256 <takeshun1619@gmail.com>
2025-01-23 14:19:42 +0900

    first commit

---
+++ app/layput.tsx
@@ -0,0 +1,14 @@
+[200~import React from "react"
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="ja">
+      <body>{children}</body>
+    </html>
+  )
+}
+
---
+++ app/page.tsx
@@ -0,0 +1,6 @@
+import React from "react"
+
+export default function Page() {
+  return <h1>Hello World</h1>
+}
+
