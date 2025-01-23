84b4c2d08df4e174f853b065a0db79cd7894d32b
takeshun256 <takeshun1619@gmail.com>
2025-01-23 18:20:26 +0900

    fix typo

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,6 +1,6 @@
 const Hero = () => (
   <section className="hero">
-    <h1>Hello, I'm a takeshun256</h1>
+    <h1>Hello, I'm takeshun256</h1>
     <p>
       東京理科大学大学院修士課程在学中のデータサイエンスおよび機械学習を専攻している学生です。
     </p>
